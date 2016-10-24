887cde874660d69a4a6a0f15841da98cce9e4bca
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2016-10-24 10:56:56 +0200

    fixup! Do not fail on missing bonmin

--- docs/examples/python/lotka_volterra_minlp.py
+++ docs/examples/python/lotka_volterra_minlp.py
@@ -148,7 +148,7 @@ if NlpSolver.hasPlugin("bonmin"):
   nlp_solver = NlpSolver('nlp_solver', 'bonmin', nlp_prob, {"discrete": discrete})
 else:
   #nlp_solver = nlpsol('nlp_solver', 'knitro', nlp_prob, {"discrete": discrete})
-  nlp_solver = nlpsol('nlp_solver', 'ipopt', nlp_prob) # Solve relaxed problem
+  nlp_solver = NlpSolver('nlp_solver', 'ipopt', nlp_prob) # Solve relaxed problem
 
 # Plot the solution
 tgrid = [T/N*k for k in range(N+1)]
